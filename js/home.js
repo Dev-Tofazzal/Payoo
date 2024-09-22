0481e62c6eaf426e0f16e11d1baa308f1675d5a1
mdtofazzalislam <dev.tofazzal122@gmail.com>
2024-09-22 18:28:58 +0600

    add new balance

--- js/home.js
+++ js/home.js
@@ -3,11 +3,26 @@ document.getElementById("addMoney").addEventListener("click",function(event){
 
    event.preventDefault(); 
    const addMoneyInput = document.getElementById("addMoneyInput").value ; 
-   console.log(addMoneyInput);
+   
    
    const pinNumberInput = document.getElementById("pinNumberInput").value;
-   console.log(pinNumberInput);
+  
    
+   if(pinNumberInput ===  "1234" ){
+    
+    const balance = document.getElementById("accountBalance").innerText;
+    
+   const addMonerNumber = parseFloat(addMoneyInput);
+   const balanceNumber = parseFloat(balance);
+    
+   const  newbalance = addMonerNumber + balanceNumber;
+   console.log(newbalance);
    
+   document.getElementById("accountBalance").innerText = newbalance; 
+    
+   }
+   else{
+    alert("Failed to add money ! please try again")
+   }
     
 })
