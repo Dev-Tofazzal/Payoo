83c5cb57f25179aeafa1443446071ce7a59eb59a
mdtofazzalislam <dev.tofazzal122@gmail.com>
2024-09-22 11:23:01 +0600

    first commit

---
+++ js/logIn.js
@@ -0,0 +1,10 @@
+document.getElementById("logInBtn").addEventListener("click",function(event){
+    
+    event.preventDefault();
+    console.log("click hoise");
+
+    const phoneNumber= document.getElementById("phoneNumber").value;
+    console.log(phoneNumber);
+    
+    
+})
